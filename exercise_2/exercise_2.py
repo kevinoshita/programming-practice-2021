037116ac440095ee0e8cb8a0594fb01733b64a3a
kevinoshita <67143054+kevinoshita@users.noreply.github.com>
2021-05-09 21:37:27 +0900

    current progress in homework

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -1,9 +1,34 @@
 def exercise_2(inputs): # DO NOT CHANGE THIS LINE
     """
-    This functions receives the input in the parameter 'inputs'. 
-    Change the code, so that the output is sqaure of the given input.
+    class Party:
+    info_attendees = {}
+    detailed_info_attendees = {}
 
-    Output should be the name of the class.
+    def add_attendees(self, family_name, number_of_attendees):
+        self.family_name = family_name
+        self.number_of_attendees = number_of_attendees
+        self.info_attendees[self.family_name] = number_of_attendees
+    def detailed_attendees(self, family_names, adult_attendees, child_attendees):
+        self.detailed_info_attendees[family_names] = (adult_attendees, child_attendees)
+    def check_and_resolve(self):
+        value = {k: self.detailed_info_attendees[k] for k in set(self.detailed_info_attendees) - set(self.info_attendees)}
+        if value == {}:
+            print("no difference")
+        else:
+            print("there is difference")
+    def display(self):
+        return self.info_attendees
+    def display_detailed(self):
+        return self.detailed_info_attendees
+    def get_total_attendees(self):
+        number_of_attendees = [len(v) for v in self.detailed_info_attendees.values()]
+        total = sum(number_of_attendees)
+        print(total)
+    def filter_attendees(self):
+        if self.child_attendees == True:
+            print(self.detailed_info_attendees[self.family_names],"has a child")
+        elif self.child_attendees == False:
+            print("no children")
     """
     output = inputs
 
